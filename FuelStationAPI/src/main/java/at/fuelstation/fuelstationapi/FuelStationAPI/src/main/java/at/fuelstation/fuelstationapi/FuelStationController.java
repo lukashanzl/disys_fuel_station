74ff81fc735a875f6a45042879a29d520a22f73e
Stefan <131394134+Kakaobaum@users.noreply.github.com>
2024-06-21 22:42:09 +0200

    Edit API -> Producer for 1st message
    Edit Producer so the Message lands in the right queue 
    The first message that starts the process sent successfully.

---
+++ FuelStationAPI/src/main/java/at/fuelstation/fuelstationapi/FuelStationAPI/src/main/java/at/fuelstation/fuelstationapi/FuelStationController.java
@@ -0,0 +1,49 @@
+package at.fuelstation.fuelstationapi;
+
+import at.fuelstation.fuelstationapi.FuelStationService;
+import at.fuelstation.fuelstationapi.communication.MessageSender;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.UnsupportedEncodingException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import at.fuelstation.fuelstationapi.FuelStationService;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+
+@RestController
+@RequestMapping("/invoices")
+public class FuelStationController {
+
+    private final static String BROKER_URL = "localhost";
+
+    @Autowired
+    private FuelStationService invoiceService;
+
+    @PostMapping("/{customerId}")
+    public ResponseEntity<String> generateInvoice(@PathVariable String customerId) throws UnsupportedEncodingException {
+        invoiceService.startDataGatheringJob(customerId);
+        return ResponseEntity.ok("Invoice generation started for customer ID: " + customerId);
+    }
+
+    /*
+    @GetMapping("/{customerId}")
+    public ResponseEntity<?> getInvoice(@PathVariable String customerId) {
+        // fetch PDF
+    }
+    */
+
+}
---
+++ FuelStationAPI/src/main/java/at/fuelstation/fuelstationapi/FuelStationAPI/src/test/java/at/fuelstation/fuelstationapi/FuelStationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fuelstation.fuelstationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuelStationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
